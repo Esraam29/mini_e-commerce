b21a904b5dd3bf4048e47df5aeca9662cf487c8e
Esraa Mohamed <esraamohamed0101650@gmail.com>
2026-09-16 22:43:06 +0300

    Authorization ( Admin middleware )

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+// Route::middleware(['auth', 'isAdmin'])->group(function () {
+//    admin routes
+// });
 require __DIR__.'/auth.php';
